1b7db2a74d50c09046324f6f98fce415befdc2d6
Stephen <anil.stephen66@gmail.com>
2019-04-10 17:41:26 +0530

    added header4

--- functions.php
+++ functions.php
@@ -16,3 +16,32 @@ add_filter( 'sp_list_google_fonts', function( $fonts ){
   );
   return $fonts;
 } );
+
+/* CHANGE THE ATTRIBUTES PASSED TO THE NAVIGATION MENU */
+add_filter('sp_nav_menu_options', function( $sp_nav_menu_options ){
+
+  global $sp_customize;
+
+  $header_type = $sp_customize->get_header_type();
+
+  if( $header_type == 'header4' ){
+
+    $sp_nav_menu_options['container_class'] = 'navbar-collapse collapse';
+    $sp_nav_menu_options['container_id']    = 'bs-example-navbar-collapse-1';
+    $sp_nav_menu_options['menu_class']      = 'nav navbar-nav top-buffer';
+
+  }
+
+  return $sp_nav_menu_options;
+});
+
+// HEADER OPTIONS
+add_filter( 'sp_headers_options', function( $headers_arr ){
+  $headers_arr['header4'] = 'Sarai Menu';
+  return $headers_arr;
+} );
+
+add_filter( 'sp_header4_template', function( $template ){
+  $template = get_stylesheet_directory().'/partials/header4.php';
+  return $template;
+} );
---
+++ partials/header4.php
@@ -0,0 +1,31 @@
+<div id="gallery" class="container">
+  <!-- <div id="g_title"> -->
+    Gallery Space
+  <!-- </div> -->
+</div>
+
+<div id="social-main" class="container">
+      <?php do_action('sp_logo'); ?>
+      <ul class="pull-right">
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+        <li><a href="#"><i class="fa fa-search"></i></a></li>
+      </ul>
+</div>
+
+<nav class="navbar header4">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<?php do_action('sp_nav_menu');?>
+
+	</div>
+	<!-- /.container-fluid -->
+</nav>
