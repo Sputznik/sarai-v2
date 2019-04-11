30b02782a001f38ba8e2439ea55c95e21927ef23
Stephen <anil.stephen66@gmail.com>
2019-04-11 10:26:16 +0530

    added new styles to elements

--- partials/header4.php
+++ partials/header4.php
@@ -1,20 +1,19 @@
 <div id="gallery" class="container">
-  <!-- <div id="g_title"> -->
     Gallery Space
-  <!-- </div> -->
 </div>
 
 <div id="social-main" class="container">
       <?php do_action('sp_logo'); ?>
-      <ul class="pull-right">
-        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-        <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-        <li><a href="#"><i class="fa fa-search"></i></a></li>
-      </ul>
+  <div class="social">
+    <ul class="list-inline pull-right">
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+      <li><a href="#"><i class="fa fa-search"></i></a></li>
+    </ul>
+  </div>
 </div>
-
 <nav class="navbar header4">
 	<div class="container">
 		<div class="navbar-header">
@@ -23,9 +22,8 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
+      <?php do_action('sp_logo'); ?>
 		</div>
 		<?php do_action('sp_nav_menu');?>
-
 	</div>
-	<!-- /.container-fluid -->
 </nav>
