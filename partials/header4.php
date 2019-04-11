8dbaaaf88cd34aa9745cfc9c80dbfdd89700496a
Stephen <anil.stephen66@gmail.com>
2019-04-11 16:34:38 +0530

    updated styles

--- partials/header4.php
+++ partials/header4.php
@@ -14,16 +14,16 @@
     </ul>
   </div>
 </div>
-<nav class="navbar header4">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-      <?php do_action('sp_logo'); ?>
-		</div>
-		<?php do_action('sp_nav_menu');?>
-	</div>
-</nav>
+  <nav class="navbar header4">
+  	<div class="container">
+  		<div class="navbar-header">
+  			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
+  				<span class="icon-bar"></span>
+  				<span class="icon-bar"></span>
+  				<span class="icon-bar"></span>
+  			</button>
+        <?php do_action('sp_logo'); ?>
+  		</div>
+  		<?php do_action('sp_nav_menu');?>
+  	</div>
+  </nav>
