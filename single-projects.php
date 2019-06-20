0e32168500c04338d6fac4bb40f42379f8cf0591
unknown <samvthom16@gmail.com>
2019-06-20 19:27:53 +0530

    Update css for templates - alignment

--- single-projects.php
+++ single-projects.php
@@ -2,8 +2,8 @@
 <div class="container" id="single-post">
   <?php if( have_posts() ): while( have_posts() ): the_post();?>
   <div class="row">
-    <div class="col-md-8 col-xs-12">
-      <h5 style="margin-top: 26px;color:#777777;">Project</h5>
+    <div class="col-md-7 col-xs-12">
+      <h5 style="margin-top: 25px;color:#777777;">Project</h5>
       <h2 class="project-title"><?php the_title(); ?></h2>
       <!-- Featured Image -->
       <?php if( has_post_thumbnail() ):?>
@@ -13,11 +13,8 @@
       <div class="project-content" style="margin-bottom:100px;"><?php the_content(); ?></div>
       <?php endwhile;endif; ?>
     </div>
-    <div class="col-md-3 col-md-offset-1 col-xs-12" id="projects-sidebar">
-      <?php if( is_active_sidebar( 'projects' ) ){
-            dynamic_sidebar( 'projects' );
-      }
-      ?>
+    <div class="col-md-4 col-md-offset-1 col-xs-12" id="projects-sidebar" style="padding-top:25px;">
+      <?php if( is_active_sidebar( 'projects' ) ){ dynamic_sidebar( 'projects' ); }?>
     </div>
   </div>
 </div>
