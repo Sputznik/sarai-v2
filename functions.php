7a680efb190a9b3147da7112c033cb8f1df43d3c
Steve <anil.stephen66@gmail.com>
2019-07-23 17:23:27 +0530

    Added site description to the header

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@ add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style( 'sarai', get_stylesheet_directory_uri() .'/assets/css/sarai.css', array( 'sarai-child' ), '1.0.7' );
 });
 
+add_action('sp_header', function(){
+  _e( '<div style="background-color:#efefef;padding:4px;"><div class="container" >'.get_bloginfo( 'description' ).'</div></div>' );
+},99);
 
 // Include Custom Post Type
 include( get_stylesheet_directory().'/cpt/cpt.php' );
@@ -26,33 +29,33 @@ add_filter( 'sp_list_google_fonts', function( $fonts ){
 } );
 
 /* CHANGE THE ATTRIBUTES PASSED TO THE NAVIGATION MENU */
-add_filter('sp_nav_menu_options', function( $sp_nav_menu_options ){
-
-  global $sp_customize;
-
-  $header_type = $sp_customize->get_header_type();
-
-  if( $header_type == 'header4' ){
-
-    $sp_nav_menu_options['container_class'] = 'collapse navbar-collapse';
-    $sp_nav_menu_options['container_id']    = 'bs-example-navbar-collapse-1';
-    $sp_nav_menu_options['menu_class']      = 'nav navbar-nav navbar-right';
-
-  }
-
-  return $sp_nav_menu_options;
-});
+// add_filter('sp_nav_menu_options', function( $sp_nav_menu_options ){
+//
+//   global $sp_customize;
+//
+//   $header_type = $sp_customize->get_header_type();
+//
+//   if( $header_type == 'header4' ){
+//
+//     $sp_nav_menu_options['container_class'] = 'collapse navbar-collapse';
+//     $sp_nav_menu_options['container_id']    = 'bs-example-navbar-collapse-1';
+//     $sp_nav_menu_options['menu_class']      = 'nav navbar-nav navbar-right';
+//
+//   }
+//
+//   return $sp_nav_menu_options;
+// });
 
 // HEADER OPTIONS
-add_filter( 'sp_headers_options', function( $headers_arr ){
-  $headers_arr['header4'] = 'Sarai Menu';
-  return $headers_arr;
-} );
+// add_filter( 'sp_headers_options', function( $headers_arr ){
+//   $headers_arr['header4'] = 'Sarai Menu';
+//   return $headers_arr;
+// } );
 
-add_filter( 'sp_header4_template', function( $template ){
-  $template = get_stylesheet_directory().'/partials/header4.php';
-  return $template;
-} );
+// add_filter( 'sp_header4_template', function( $template ){
+//   $template = get_stylesheet_directory().'/partials/header4.php';
+//   return $template;
+// } );
 
 // Register side bar for Gallery Space
 add_action( 'widgets_init', function(){
@@ -81,6 +84,16 @@ add_action( 'widgets_init', function(){
     'name' 			    => 'Events',
     'id' 			      => 'events',
     'description' 	=> 'Shows all event\'s',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' 	=> '</aside>',
+    'before_title' 	=> '<h3 class="widget-title">',
+    'after_title' 	=> '</h3>',
+  ) );
+
+  register_sidebar( array(
+    'name' 			    => 'Sidebar for Single Post',
+    'id' 			      => 'single-post-sidebar',
+    'description' 	=> 'Visible for single posts only',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' 	=> '</aside>',
     'before_title' 	=> '<h3 class="widget-title">',
