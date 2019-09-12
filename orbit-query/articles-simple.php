afeb5d3e29e1235dbebeab8989354cb8867e4489
unknown <samvthom16@gmail.com>
2019-09-12 11:39:34 +0530

    Update and refactor styles

--- orbit-query/articles-simple.php
+++ orbit-query/articles-simple.php
@@ -1,8 +1,7 @@
 <ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
 	<li class="orbit-article" style="margin-bottom:25px;">
-    <!-- <h4 style="margin-bottom: 5px;"><a href="<?php //the_permalink();?>"><?php //the_title();?></a></h4> -->
-    <p><?php the_title();?></p>
+    <p><a href="<?php the_permalink();?>"><?php the_title();?></a></p>
   </li>
   <?php endwhile;?>
 </ul>
